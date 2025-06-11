d47d77f78fb96eba7abc7162854c6278759869ad
Wiall <anastasia0601voyt@gmail.com>
2025-06-11 15:23:05 +0300

    0.0.1

---
+++ chatappbackend/Migrations/20250610101620_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace chatappbackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MessageText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnalysisResults",
+                columns: table => new
+                {
+                    ResultID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MessageID = table.Column<int>(type: "int", nullable: false),
+                    AnalysisDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sentiment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnalysisResults", x => x.ResultID);
+                    table.ForeignKey(
+                        name: "FK_AnalysisResults_Messages_MessageID",
+                        column: x => x.MessageID,
+                        principalTable: "Messages",
+                        principalColumn: "MessageID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnalysisResults_MessageID",
+                table: "AnalysisResults",
+                column: "MessageID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnalysisResults");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
